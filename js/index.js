e8d75c24d47188954b32d11b5813237196ecef30
OmegaKi113r <omegaki113r@gmail.com>
2021-06-11 10:11:15 +0530

    final commit

--- js/index.js
+++ js/index.js
@@ -7,9 +7,40 @@ const linkedin_link = "https://linkedin.com";
 var SCREEN_WIDTH = screen.width;
 var slideIndex = 0;
 var isSearchExpanded = false;
+var inputChanged = false;
 
+var RunningTimer = null;
 
 showSlides();
+
+function timer(callback, delay) {
+    var id, started, remaining = delay, running
+    this.start = function() {
+        running = true
+        started = new Date()
+        id = setTimeout(callback, remaining)
+    }
+    this.pause = function() {
+        running = false
+        clearTimeout(id)
+        remaining -= new Date() - started
+    }
+    this.getTimeLeft = function() {
+        if (running) {
+            this.pause()
+            this.start()
+        }
+        return remaining
+    }
+    this.getStateRunning = function() {
+        return running
+    }
+    this.stop = function(){
+        clearTimeout(id);
+    }
+
+    this.start()
+}
 
 function showSlides() {
     var i;
@@ -54,6 +85,10 @@ function openNewTab(link){
         newTab.location;
 }
 
+function handleInputChanged(){
+    collapseSearch();
+}
+
 $(document).ready(function(){
     console.log("document ready");
     $(".logo_container").click(function () {
@@ -65,21 +100,107 @@ $(document).ready(function(){
         SCREEN_WIDTH = screen.width;
         if(SCREEN_WIDTH >= 768){
         }else{
-            collapseSearch();
+            var inputText = $(".search_input").val();
+            if(inputText == ""){
+                $(".search_input").val("");
+                collapseSearch();
+            }else{
+
+            }
+            
+            
         }
+    });
+    $(".search_input").on('input',function(){
+        console.log("change event");
+        SCREEN_WIDTH = screen.width;
+        if(SCREEN_WIDTH >= 768){
+
+        }else{
+            inputChanged = true;
+            if(RunningTimer == null){
+                RunningTimer = new timer(handleInputChanged,8000);
+            }else{
+                RunningTimer.stop();
+                RunningTimer = null;
+                RunningTimer = new timer(handleInputChanged,8000);
+            }
+            
+        }
+        
     });
     $(".image_button_container").click(function(event){
         SCREEN_WIDTH = screen.width;
         if(SCREEN_WIDTH >= 768){
             //this means website is running either on tablet or PC
             console.log("search button on tablet or PC");
+            var searchText = $(".search_input").val().trim();
+            if(searchText == ""){
+                // user hasn't inserted any text
+                console.log("no values entered");
+            }else{
+                // move user to the requested page
+                switch(searchText.toLowerCase()){
+                    case "home":
+                        $(".search_input").val("");
+                        window.location = "index.html";
+                        break;
+                    case "gallery":
+                        $(".search_input").val("");
+                        window.location = "gallery.html";
+                        break;
+                    case "about":
+                        $(".search_input").val("");
+                        window.location = "about.html"
+                        break;
+                    default:
+                        console.log("User entered a unknown value.. handle it here");
+                        alert("Page Not Found");
+                        break;
+                }
+            }
+            console.log(searchText);
+
         }else{
             // this means website is running on mobile
             console.log("search button on mobile");
             if(isSearchExpanded){
-                collapseSearch();
+                var searchText = $(".search_input").val().trim();
+                if(searchText == ""){
+                    // user hasn't inserted any text
+                    console.log("no values entered");
+                    $(".search_input").val("");
+                    collapseSearch();
+                }else{
+                    // move user to the requested page
+                    switch(searchText.toLowerCase()){
+                        case "home":
+                            $(".search_input").val("");
+                            window.location = "index.html";
+                            break;
+                        case "gallery":
+                            $(".search_input").val("");
+                            window.location = "gallery.html";
+                            break;
+                        case "about":
+                            $(".search_input").val("");
+                            window.location = "about.html"
+                            break;
+                        default:
+                            console.log("User entered a unknown value.. handle it here");
+                            alert("Page Not Found");
+                            break;
+                    }
+                }
             }else{
                 expandSearch();
+                if(RunningTimer == null){
+                    RunningTimer = new timer(handleInputChanged,8000);
+                }else{
+                    RunningTimer.stop();
+                    RunningTimer = null;
+                    RunningTimer = new timer(handleInputChanged,8000);
+                }
             }
         }
         
